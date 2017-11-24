67a07a1633f5f5645ca1d4a788ff2234056c1cf6
thomas <thomasgaz@hotmail.com>
2017-11-24 10:49:39 +0000

    initial commit

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,15 @@ import axios from "axios";
 
 export default {
     addNew: stock=>
-           axios.post("https://tgstockmarket-gkazikas.c9users.io/addNew", { stock }).then(res => res.data.data),
+           axios.post("https://cors-anywhere.herokuapp.com/https://tgstockmarket-gkazikas.c9users.io/addNew", { stock }).then(res => res.data.data),
     fetch: () =>
-            axios.post("https://tgstockmarket-gkazikas.c9users.io/fetcStocks").then(res => res.data.stocks)
+            axios.post("https://cors-anywhere.herokuapp.com/https://tgstockmarket-gkazikas.c9users.io/fetcStocks").then(res => res.data.stocks)
 };
+// import axios from "axios";
+// const host = window.location.hostname;
+// export default {
+//     addNew: stock=>
+//           axios.post(`https://${host}/addNew`, { stock }).then(res => res.data.data),
+//     fetch: () =>
+//             axios.post(`https://${host}/fetcStocks`).then(res => res.data.stocks)
+// };
